e0b2d0018b7dfd83154e1aa350ae58e2ed69e1b4
icelly_QAQ <1787522500@qq.com>
2024-10-12 23:00:02 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-ADB_Write
+# ADB_Write
+**该项目是基于pyhton制作的adb模拟手写输入的程序**
 
+****
 
-该项目是基于pyhton制作的adb模拟手写输入的程序
+### 食用方法：
+
+**get_str_xy(input_str, shift_x, shift_y, more) # 函数将会返回可以直接在adb shell中运行的命令**
+
++ input_str：需要处理的字符（str类型，当 more 传入 True 时，需要传入列表）
+
++ shift_x：X偏移量（int类型，默认值为 0）
+
++ shift_y：Y偏移量（int类型，默认值为 0）
+
++ more：是否启用多字符处理（bool类型，默认值为 False）
+
+**Tips：偏移量从屏幕左上角开始计算（左上角为（0, 0）向右为+X， 向下为+Y）**
--- str_xy_lib.py
+++ str_xy_lib.py
@@ -9,22 +9,14 @@ base_coordinates = {
     "4": [[50, 10], [10, 50], [110, 50], [90, 50], [90, 20], [90, 110]],
     "5": [[10, 10], [10, 60], [60, 80], [10, 110], [60, 80], [10, 60], [10, 25], [80, 25]],
     "6": [[80, 10], [10, 90], [50, 130], [100, 90], [50, 70]],
-    ">": [[10, 10], [50, 50], [10, 10]],
+    "7": [[10, 10], [60, 10], [30, 90]],
+    "8": [[10, 40], [50, 80], [25, 110], [10, 80], [50, 40], [25, 15], [10, 40]],
+    "9": [[50, 10], [10, 10], [10, 50], [50, 50], [50, 10], [50, 110]],
+    "0": [[25, 10], [10, 25], [10, 75], [25, 100], [50, 75], [50, 25], [25, 10]],
+    ">": [[10, 10], [50, 50], [10, 100]],
     "<": [[50, 10], [10, 50], [50, 100]],
     "=": [[1284, 1122], [1700, 1122], [1280, 1300], [1700, 1300]]
 }
-
-
-@lru_cache()
-def get_device_resolution():
-    result = subprocess.run(["adb", "shell", "wm", "size"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    output = result.stdout
-    if "Physical size" in output:
-        resolution_str = output.split(":")[-1].strip()
-        width, height = map(int, resolution_str.split("x"))
-        return width, height
-    else:
-        raise Exception("无法获取设备分辨率")
 
 
 def get_str_xy(input_str, shift_x: int = 0, shift_y: int = 0, more=False):
@@ -48,8 +40,8 @@ def get_str_xy(input_str, shift_x: int = 0, shift_y: int = 0, more=False):
 '''
 
 if __name__ == "__main__":
-    str_to_get = "1"
-    shift_x = 600
-    shift_y = 100
+    str_to_get = "9"
+    shift_x = 100
+    shift_y = 500
     for i in range(len(get_str_xy(str_to_get, shift_x, shift_y))):
         awa = subprocess.run(["adb", "shell", get_str_xy(str_to_get, shift_x, shift_x)[i]])
